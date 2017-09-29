37f9c6be2cd84f106d6134c9d491ff62d48b9c91
Justin <ChoppingBroccoli@users.noreply.github.com>
2017-09-29 13:28:50 -0400

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,18 @@
+'''
+Takes an integer from the user then multiply all integers from 1 through n.
+EXAMPLE: digit_fact(4) would be 4 * 3 * 2 * 1 and the product printed to the screen
+'''
+
+def digit_fact(n):
+
+    #  Assign n to next_number so we can work with it
+    next_number = n
+
+
+    next_number = next_number - n
+
+    #  need a loop to determine the numbers from n - 1 and put into a list
+    
+
+    #  Print the sum to the screen
+    print("the sum here")
